5e72ea525eb5aea158079046f5552f6396719565
Wengon24 <wendy.gonzalezbonilla@ucr.ac.cr>
2025-06-25 23:51:41 -0600

    InsertarMaterial_WendyGonzalez_Ruta: Route::post('/insertar_materiales', [App\Http\Controllers\MaterialController::class,'store']);

---
+++ app/Http/Controllers/MaterialController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Material;
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class MaterialController extends Controller
+{
+  
+
+    public function store(Request $request)
+{
+    $validator = Validator::make($request->all(), [
+        'unidad_medida' => 'required|string|max:50',
+        'descripcion' => 'required|string|max:255',
+        'ubicacion' => 'required|string|max:100',
+        'categoria_id' => 'required|exists:categorias,id'
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json([
+            'success' => false,
+            'errors' => $validator->errors()
+        ], 422);
+    }
+
+    $material = Material::create($request->all());
+
+    return response()->json([
+        'success' => true,
+        'message' => 'Material creado correctamente.',
+        'data' => $material
+    ], 201);
+}
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/insertar_materiales', [App\Http\Controllers\MaterialController::class,'store']);
